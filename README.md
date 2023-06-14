a322fa053f8bdd649de8e91ee4d5b8d769d28996
“Vesna” <projectappvesna@gmail.com>
2023-06-14 13:39:32 +0100

    Added class, option 3 and a bit reorganized a code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # _Project Name:_ Simple calculator that uses defensive programming
+
+> **Table of Contents:**
+> * Project description
+> * Code description
+> * Usage
+
+#### **Project description:**
+The program contains several functions, it is a basic framework for a simple calculator program that can perform arithmetic equations interactively and read equations from a file, displaying the results and storing them in a file named equations.txt. It demonstrates the use of functions, exception handling, file input/output operations, and user input validation.
+
+#### **Project description:**
+The program contains several functions, a basic framework for a simple calculator program that can perform arithmetic equations interactively and read equations from a file, displaying the results and storing them in a file named equations.txt. It demonstrates the use of functions, exception handling, file input/output operations, and user input validation.
+
+#### **Code description:**
+1. The code begins by importing the os module, which provides functions for interacting with the operating system.
+2. A custom exception `class InvalidOperatorError` is defined to handle invalid operator inputs.
+3. The `performArithmeticOperation():` function takes three arguments: `firstNumber`, `secondNumber`, and `operator`. It performs basic arithmetic operations based on the operator provided and returns the result.
+4. The `loadEquationsFromFile():` function takes a file `filePath` as an argument (as input), which represents the path to a file containing equations. It first checks if the file exists using `os.path.exists()` and `raises` a `FileNotFoundError` if it does not exist. If the file exists, it proceeds to open the file and read each line.
+ - For each line in the file, it splits the line into a list of strings using `line.strip().split()`. Each element in the list represents a part of the equation: `firstNumber`, `operator`, and `secondNumber`.
+- It tries to perform the arithmetic operation by calling the `performArithmeticOperation()` function with the parsed values. If the operation succeeds, it appends the formatted equation to the `formattedEquations` list.
+- If an exception occurs during the arithmetic operation, it appends an error message to the `formattedEquations` list.
+ - After processing all the lines in the file, it prints a message indicating that reading from the file is complete and returns the `formattedEquations` list.
+5. The `simpleCalculatorInteractive():` function sets up an interactive calculator interface using a `while loop`. It prompts the user for `input`, allowing them to choose an option: to perform a calculation, read equations from a file, or exit the program. Based on the user's input, it performs the corresponding actions.
+- If the user selects option "1", it prompts the user to enter the first number, second number, and operator. It then calls the `performArithmeticOperation()` function to perform the calculation and prints the result.
+- It appends the equation and result to a file named "equations.txt" using the `open()` function in `append` mode.
+- If the user selects option "2", it prompts the user to enter the file name, and then calls the `loadEquationsFromFile()` function with the provided file name and prints the formatted equations.
+- If the user selects option "3", it asks the user if they want to quit the program. If the user responds with "yes", the program breaks out of the while loop and ends. Otherwise, it continues the loop.
+6. The code uses exception handling to catch and handle potential errors. 
+- If the user enters an invalid choice, it raises a `ValueError` and prints an error message.
+- If a `ZeroDivisionError` for division by zero, occurs during the arithmetic operation, it prints an error message.
+- If any other `Exception` occurs, it prints an error message is displayed to the user.
+7. After each iteration of the `while loop`, it prints a message indicating that the operation is complete.
+8. The `if __name__ == "__main__":` condition ensures that the `simpleCalculatorInteractive` function is only executed when the script is run directly and not when imported as a module.
+
+#### **Usage:**
+The program demonstrates a basic calculator program which handles error handling and files operations.
+You can further expand or customize it to suit your specific needs, or integrate it into bigger projects.
--- simple_calculator.py
+++ simple_calculator.py
@@ -1,32 +1,29 @@
+# import the os module, check if a file exists
 import os
 
 
-# using the 'map' to unpack the equation list into 3 separate variables (firstNumber, operator, secondNumber) instead of indexing each element separately
-# added a 'finally' block to 'loadEquationsFromFile' and 'simpleCalculatorInteractive', 
-# but finally block in 'performArithmeticOperation' it is not necessary because there no resources to be open that need to be closed
-
-
+class InvalidOperatorError(Exception):
+    pass
 
 def performArithmeticOperation(firstNumber, secondNumber, operator):
-    try:
-        if operator == "+":
-            return firstNumber + secondNumber
-        elif operator == "-":
-            return firstNumber - secondNumber
-        elif operator == "*":
-            return firstNumber * secondNumber
-        elif operator == "/":
-            if secondNumber == 0:
-                raise ZeroDivisionError("Cannot divide by zero./n")
-            return firstNumber / secondNumber
-        else:
-            raise ValueError("Invalid operator!")
-    except Exception as ex:
-        return f"Error: {ex}"
-
+    if operator == "+":
+        return firstNumber + secondNumber
+    elif operator == "-":
+        return firstNumber - secondNumber
+    elif operator == "*":
+        return firstNumber * secondNumber
+    elif operator == "/":
+        if secondNumber == 0:
+            raise ZeroDivisionError("Incorrect entry! Cannot be divided by zero.")
+        return firstNumber / secondNumber
+    else:
+        raise InvalidOperatorError("Invalid operator!")
 
 
 def loadEquationsFromFile(filePath):
+    if not os.path.exists(filePath):
+        raise FileNotFoundError("File not found!")
+    
     formattedEquations = []
     try:
         with open(filePath, "r") as my_file:
@@ -39,37 +36,47 @@ def loadEquationsFromFile(filePath):
                 except Exception as ex:
                     formattedEquations.append(f"{firstNumber} {operator} {secondNumber} = Error ({ex})")
         return formattedEquations
-    except FileNotFoundError:
-        print("File not found!")
-        return []
     finally:
         print("Reading from file is complete.")
-        
 
 
 def simpleCalculatorInteractive():
     while True:
         try:
-            userOption = input(
-                "Please enter Option: '1' to calculate, or '2' to read from file:\n > ")
+            # prompt user for input
+            userOption = input("Please enter Option: '1' to calculate, or '2' to read from file, or '3' to exit:\n > ")
+
             if userOption == "1":
+                # perform arithmetic calculation
                 firstNumber = int(input("Please enter the 1st number:\n > "))
                 secondNumber = int(input("Please enter the 2nd number:\n > "))
-                operator = input(
-                    "Please enter the operator (+, -, *, /):\n > ")
+                operator = input("Please enter the operator (+, -, *, /):\n > ")
                 result = performArithmeticOperation(firstNumber, secondNumber, operator)
                 print(f"{firstNumber} {operator} {secondNumber} = {result}")
-                with open('equations.txt', "a") as my_file:
-                    my_file.write(
-                        f"{firstNumber} {operator} {secondNumber} = {result}\n")
-                    print("File created and calculations written to the file.")
+
+                # write calculation to file
+                with open("equations.txt", "a") as my_file:
+                    my_file.write(f"{firstNumber} {operator} {secondNumber} = {result}\n")
+                print("File created and calculations written to the file.\n")
+
             elif userOption == "2":
+                # load equations from file and display results
                 filePath = input("Please enter the file name:\n > ")
                 equations = loadEquationsFromFile(filePath)
                 for equationResult in equations:
                     print(equationResult)
+
+            elif userOption == "3":
+                # exit the program
+                userQuit = input("Would you like to quit the program? (Yes/No):\n > ")
+                if userQuit.lower() == "yes":
+                    break
+                else:
+                    continue
+
             else:
                 raise ValueError("Invalid choice!")
+            
         except ValueError as ve:
             print(f"Invalid input: {ve}")
         except ZeroDivisionError as zde:
@@ -79,8 +86,8 @@ def simpleCalculatorInteractive():
         finally:
             print("The operation is complete.\n")
 
+# simpleCalculatorInteractive()
 
-# this checks whether the script is being executed as the 'main' code, and calls the 'simpleCalculatorInteractive' to start the main program loop
 if __name__ == "__main__":
     simpleCalculatorInteractive()
     
